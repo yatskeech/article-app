e71c4c143a7f85cf44b727db591a0067f7628a6c
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-03-02 16:44:14 +0300

    feat: add fast refresh

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,12 +2,20 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
 
 export function buildPlugins(options: BuildOptions): webpack.WebpackPluginInstance[] {
-  return [
+  const plugins = [
     new webpack.ProgressPlugin(),
     new HtmlWebpackPlugin({ template: options.paths.html }),
     new MiniCssExtractPlugin({ filename: 'css/[name].css' }),
     new webpack.DefinePlugin({ IS_DEV: options.isDev }),
-  ];
+  ]
+
+  if (options.isDev) {
+    plugins.push(new ReactRefreshWebpackPlugin());
+  }
+
+  return plugins;
 }
